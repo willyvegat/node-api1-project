dd1ad55dcf0014aa1765e5127f1b444db54d32d5
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-05 22:25:37 -0400

    added packages and server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+
+const server = express();
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'testing end point but it is not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
