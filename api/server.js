f6f85a11ef1c1030c05a7e124d197aac0d1a328c
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-06 00:35:34 -0400

    put endpoint created and passed

--- api/server.js
+++ api/server.js
@@ -57,20 +57,52 @@ server.post('/api/users', (req, res) => {
 })
 
 server.delete('/api/users/:id', async (req, res) => {
-    const removeUser = await User.findById(req.params.id)
-    if(!removeUser) {
+    try {
+        const removeUser = await User.findById(req.params.id)
+        if(!removeUser) {
         res.status(404).json({
             message: "The user with the specified ID does not exist"
         })
-    } else {
+        } else {
         const deletedUser = await User.remove(req.params.id)
         res.status(200).json(deletedUser)
+        }
+    } catch(err) {
+        res.status(500).json({ 
+            message: "The user could not be removed",
+            err: err.message 
+        }) 
     }
 })
 
-// server.put('/api/users/:id', (req, res) => {
-//     User.update()
-// })
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if(!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch(err) {
+        res.status(500).json({ 
+            message: "The user information could not be modified",
+            err: err.message 
+        }) 
+    }
+})
 
 
 
